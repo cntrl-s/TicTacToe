dfed7a2f3352b95e6702ccc6c11186d0ab485013
foo <sandwrm@pm.me>
2020-10-17 13:49:41 +0530

    add new methods and properties

--- src/org/tictac/view/PlayerType.java
+++ src/org/tictac/view/PlayerType.java
@@ -1,5 +1,23 @@
 package org.tictac.view;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public enum PlayerType {
 	X, O;
+	
+	private List<Integer> moves;
+	
+	private PlayerType() {
+		this.moves = new ArrayList<>();
+	}
+	
+	public List<Integer> getMoves() {
+		return moves;
+	}
+
+	public void add(int move) {
+		this.moves.add(move);
+	}
+
 }
